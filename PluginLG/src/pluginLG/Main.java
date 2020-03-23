04bc4c3ed83e6b2543170aa9463c324109fe9673
Florian Spick <florianspk@gmail.com>
2020-03-23 17:07:58 +0100

    Base

---
+++ PluginLG/src/pluginLG/Main.java
@@ -0,0 +1,16 @@
+package pluginLG;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
